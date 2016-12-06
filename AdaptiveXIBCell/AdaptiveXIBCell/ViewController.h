c3a631320ea5248a8d3d415dfe3cbeac73b96a82
韩博 <15093232243@163.com>
2016-12-06 10:27:54 +0800

    初始化项目

---
+++ AdaptiveXIBCell/AdaptiveXIBCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdaptiveXIBCell
+//
+//  Created by bnysc on 2016/12/6.
+//  Copyright © 2016年 HB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
